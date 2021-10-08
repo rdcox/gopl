2aab3d6c468cd914984d8d80fcf3e4d535df813b
rdcox <rcox@6river.com>
2021-10-08 17:35:15 -0400

    finishing ex 1-6

--- ch1/exercises/ex1-6.go
+++ ch1/exercises/ex1-6.go
@@ -3,6 +3,68 @@
 // of SetColorIndex in some interesting way.
 package main
 
-func main() {
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
 
+var palette = []color.Color{
+	color.RGBA{38, 70, 83, 1},    // 264653 (Charcoal)
+	color.RGBA{40, 114, 113, 1},  // 287271 (Skobeloff)
+	color.RGBA{42, 157, 143, 1},  // 2a9d8f (Persian Green)
+	color.RGBA{138, 177, 125, 1}, // 8AB17D (Asparagus)
+	color.RGBA{233, 196, 106, 1}, // e9c46a (Orange Yellow Crayola)
+	color.RGBA{244, 162, 97, 1},  // f4a261 (Sandy Brown)
+	color.RGBA{238, 137, 89, 1},  // ee8959 (Mandarin)
+	color.RGBA{231, 111, 81, 1},  // e76f51 (Burnt Sienna)
+}
+
+const (
+	blackIndex = 0
+	greenIndex = 1
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+
+	phase := 0.0 // phase difference
+	colorIndex := 0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		if (i % (nframes / (len(palette) - 1))) == 0 {
+			if colorIndex == len(palette)-1 {
+				colorIndex = 1
+				continue
+			} else {
+				colorIndex++
+			}
+		}
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(colorIndex))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
