9632d266b515ff5a9a4bfe6e74c691a9ba117eb0
rdcox <rcox@6river.com>
2021-10-17 20:58:25 -0400

    finishing 1.7 examples and exercises

---
+++ ch1/examples/server/server1.go
@@ -0,0 +1,17 @@
+// Server1 is a minimal echo server
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8006", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
---
+++ ch1/exercises/ex1-12.go
@@ -0,0 +1,73 @@
+// handler echos the HTTP request
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+var palette = []color.Color{color.White, color.Black}
+var cycles int
+var mu sync.Mutex
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		lissajous(w)
+		mu.Unlock()
+	})
+	http.HandleFunc("/cycles", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		s := strings.Split(r.URL.RequestURI(), "=")[1]
+		newCycles, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Print(err)
+		}
+		cycles = newCycles
+		mu.Unlock()
+	})
+	log.Fatal(http.ListenAndServe("localhost:8206", nil))
+}
+
+func lissajous(out io.Writer) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+// neat results
